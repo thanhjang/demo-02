1de6cca5db619ddb2c6dd9152c198869ae3e994c
THANHJANG\ThanhJang <thanhtiny2k@gmail.com>
2021-04-12 10:52:52 +0700

    init project

---
+++ App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  Text,
+  View,
+} from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+        <View>
+           <Text>Hello World !</Text>
+        </View>
+    );
+  }
+}
